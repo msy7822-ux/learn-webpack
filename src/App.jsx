4e6dd13e176735892bea3994f6ea44dcc0d99ac2
msy7822 <msy780022@gmail.com>
2022-05-08 18:28:54 +0900

    imageをバンドルし、表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./styles.scss";
+import Favicon from './favicon.ico';
 
 export const App = () => {
   return (
     <div className="container">
+      <img src={Favicon} alt="" />
       <h1>Hello....</h1>
     </div>
   );
--- webpack.confg.js
+++ webpack.confg.js
@@ -1,39 +1,34 @@
 const path = require("node:path");
-const isDevEnv = process.env.NODE_ENV === 'development'
+const isDevEnv = process.env.NODE_ENV === 'development';
 
-// webpack とbabelを一緒に使えるようにするための設定を記述している
 module.exports = {
   target: ['web', 'es5'],
   mode: isDevEnv ? "development" : "production",
-  // ソースマップは開発環境でのみ有効にする
   devtool: isDevEnv ? "source-map" : undefined,
   devServer: {
     static: {
       directory: "./dist"
     },
   },
-  // 依存関係解決に参照するファイルの拡張子を指定
   resolve: {
     extensions: [".js", ".json", ".jsx"],
   },
-  entry: "./src/entry.jsx",
+  entry: {
+    app: "./src/entry.jsx"
+  },
   output: {
-    // ファイル名
-    filename: "bundle.js",
-    // 出力するフォルダ
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "asset/[name][ext]",
   },
   module: {
-    // 変換やバンドルのルールは module.rules 配列に指定する
-    // 多くの場合、test でファイル形式を指定し、loader（または use 配列）へローダーを指定することになる
     rules: [
       {
-        // 画像やフォントファイル
-        test: /\.(ico|png|svg|ttf|otf|eot|woff?2?)$/,
-        type: "asset",
+        // test: /\.(ico|png|svg|ttf|otf|eot|woff?2?)$/,
+        test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
-        // 拡張子 jsxのファイル（正規表現）
         test: /\.jsx?$/,
         // ローダーの指定
         loader: "babel-loader",
